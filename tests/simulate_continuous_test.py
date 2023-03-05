f1feddbf99b4549d84370964c3dac238c90c4128
Brendan Patch <brendanpatch@gmail.com>
2023-03-05 22:52:59 +1100

    Update simulate_continuous_test.py

--- tests/simulate_continuous_test.py
+++ tests/simulate_continuous_test.py
@@ -5,8 +5,7 @@ import birdepy as bd
 
 class TestUtility(unittest.TestCase):
     def test_probability_expm(self):
-        jump_times, pop_sizes = bd.simulate.continuous([0.75, 0.25, 1/1000, 0],
-											                                  'Verhulst', 10, t_max=1)
+        jump_times, pop_sizes = bd.simulate.continuous([0.75, 0.25, 1/1000, 0], 'Verhulst', 10, seed= 2021, t_max=1)
         assert np.isclose(jump_times[1], 0.1733207523280825)
         assert np.isclose(jump_times[-1], 0.9173238186709972)
         assert pop_sizes[5] == 13
