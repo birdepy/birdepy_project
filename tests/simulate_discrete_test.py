333ffe4a97cadada54ec002bafa63bf4dccab04e
Brendan Patch <brendanpatch@gmail.com>
2023-03-05 22:46:21 +1100

    Update simulate_discrete_test.py

--- tests/simulate_discrete_test.py
+++ tests/simulate_discrete_test.py
@@ -8,31 +8,31 @@ class TestUtility(unittest.TestCase):
         result = bd.simulate.discrete(1, 'Poisson', 0, times=[0, 1, 3, 4, 5], seed=2021)
 
         assert result[0] == 0
-        assert result[0] == 0
-        assert result[0] == 1
-        assert result[0] == 2
-        assert result[0] == 2
+        assert result[1] == 0
+        assert result[2] == 1
+        assert result[3] == 2
+        assert result[4 == 2
 
     def test_simulate_ea(self):
         result = bd.simulate.discrete(1, 'Poisson', 0, times=[0, 1, 3, 4, 5], method='ea', seed=2021)
         
         assert result[0] == 0
-        assert result[0] == 2
-        assert result[0] == 3
-        assert result[0] == 3
-        assert result[0] == 4
+        assert result[1] == 2
+        assert result[2] == 3
+        assert result[3] == 3
+        assert result[4] == 4
         
     def test_simulate_ma(self):
         result = bd.simulate.discrete([0.75, 0.25, 0.02, 1], 'Ricker', 10, [0, 10, 100],
                                       survival=True, seed=2021)        
         assert result[0] == 10
-        assert result[0] == 47
-        assert result[0] == 39
+        assert result[1] == 47
+        assert result[2] == 39
   
     def test_simulate_gwa(self):
         bd.simulate.discrete([0.75, 0.25, 1/1000, 0], 'Verhulst', 10,
                              [0, 10, 100], method='gwa', seed=2021)
         
         assert result[0] == 10
-        assert result[0] == 288
-        assert result[0] == 688
+        assert result[1] == 288
+        assert result[2] == 688
