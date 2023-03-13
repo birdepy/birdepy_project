a760b6555cb39a42cd56ba890f8b761b6f603c4a
Brendan Patch <brendanpatch@gmail.com>
2023-03-13 14:40:18 +1100

    Update setup.py
    added include package option to setup

--- setup.py
+++ setup.py
@@ -156,9 +156,9 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     package_data={  # Optional
-        'birdepy': ['iltcme.json'],
+        'birdepy': ['data_files/iltcme.json'],
     },
-
+    include_package_data=True,
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/distutils/setupscript.html#installing-additional-files
